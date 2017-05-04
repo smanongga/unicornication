11955830d0f183c26e5317a9960252629f2c528c
EDA <eda@Mac-Mini-7.local>
2017-05-05 11:04:55 +1200

    getFileContents now working

--- functions.js
+++ functions.js
@@ -1,11 +1,12 @@
  const fs = require('fs')
 
  function getFileContents (lib, cb) {
- fs.readFile((err, contents) => {
+ fs.readFile(lib,(err, contents) => {
    if (err) {
    return err
    }
    const data = JSON.parse(contents)
+   console.log(contents)
    cb(null, data)
      })
  }
