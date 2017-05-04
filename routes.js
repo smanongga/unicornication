a234e23d3b3f361ac28641c389e125c733b36bd9
EDA <eda@Mac-Mini-7.local>
2017-05-05 11:13:48 +1200

    getFileContents now working

--- routes.js
+++ routes.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const routes = express.Router()
-// const functions = require('./functions')
-// const lib = ("./library.json")
+ const functions = require('./functions')
+ const lib = ("./library.json")
 
 module.exports = routes
 
 
 
  routes.get('/', (req,res) => {
- functions.getFileContents('', (err,contents) =>{
+ functions.getFileContents(lib, (err,contents) =>{
  if(err){
      return res.send(err.message).status(500)
  }
  console.log(contents)
- res.render('.layouts/main', contents)
+ res.render('layouts/main', contents)
 
    })
  })
