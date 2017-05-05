87b2592af7d8c5f391abb2123417fd9872abdaaa
EDA <eda@Mac-Mini-7.local>
2017-05-05 15:15:14 +1200

    finished history function/route

--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const routes = express.Router()
+const fs = require('fs')
 
 
  const functions = require('./functions')
  const lib = ("./library.json")
+ const hist = ("./history.json")
  let name
  let path
  let findId
@@ -33,7 +35,7 @@ else if (nameNum >4 && nameNum <= 8){
 else if (nameNum > 8 && nameNum <= 12){
   result = 3
 }
-else if (nameNum > 12 && nameNum <= 16){
+else if (nameNum > 13 && nameNum <= 16){
   result = 4
 }
 else if (nameNum > 16 &&  nameNum<= 20){
@@ -45,10 +47,10 @@ else if (nameNum > 20 && nameNum <= 24){
 else if (nameNum > 24 && nameNum <= 28){
   result = 7
 }
-else if (nameNum > 28 && nameNum <= 32){
+else if (nameNum === 13){
   result = 8
 }
-else if (nameNum > 32 && nameNum <= 36){
+else if (nameNum > 28 && nameNum <= 36){
   result = 9
 }
 else if (nameNum > 36){
@@ -65,12 +67,39 @@ findId = contents.unicorns.find((unicorn) => {
   return unicorn.id === result
 })
 
+
+
+
+functions.getFileContents(hist, (err,contents) =>{
+if(err){
+    return res.send(err.message).status(500)
+}
+console.log(findId)
+findId.human = req.body.humanName
+delete findId.id
+
+ contents.submissions.push(findId)
+let edited = JSON.stringify(contents.submissions)
+edited = `{"submissions": ${edited}}`
+fs.writeFile(hist,edited,(err) =>{
+  if (err){
+    return err
+  }
+})
   })
 
   res.redirect('myunicorn')
 })
-
+})
 routes.get('/myunicorn', (req, res) =>{
   console.log(findId)
   res.render('myunicorn', findId)
+})
+
+routes.get('/history' (req,res) => {
+  functions.getFileContents(hist, (err,contents) =>{
+  if(err){
+      return res.send(err.message).status(500)
+  }
+  res.render('history', hist.submissions)
 })
